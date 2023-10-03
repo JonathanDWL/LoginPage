36d347ddfc8011d4ad855e1854dc18a55ac0100e
s10043499 <jonathan.lamping7y@stu.austinisd.org>
2023-10-03 10:35:05 -0500

    Started on LoginPage

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.app import App
 
-Builder.load_file("QuizPage.kv")
+Builder.load_file("LoginPage.kv")
+
+class LoginPageApp(App):
+    def build(self):
+        return LoginManager()
+
 class QuizPageApp(App):
     def build(self):
         return QuizManager()
 
 class QuizManager(ScreenManager):
+    pass
+
+class LoginManager(ScreenManager):
     pass
 
 class Question1Screen(Screen):
@@ -17,12 +25,15 @@ class Question1Screen(Screen):
         else:
             self.manager.current = "error"
 
+class MainScreen(Screen):
+    pass
+
 class Question2Screen(Screen):
     def answerquestion(self, text):
         if(text.lower() == "deep in the heart of texas"):
             self.manager.current = "correct"
         else:
-            self.manager.current = "error"
+            self.ids.invalid.text = "Wrong.  Try again."
 
 class CorrectScreen(Screen):
     def press(self):
@@ -32,4 +43,4 @@ class ErrorScreen(Screen):
     def press(self):
         self.manager.current = "question2"
 
-QuizPageApp().run()
+LoginPageApp().run()
